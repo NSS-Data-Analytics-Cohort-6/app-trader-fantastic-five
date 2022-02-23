cae58036f75231944e72fb46e587a5930d5aeb09
Phil George <philipwgeorge@gmail.com>
2022-02-22 22:27:41 -0500

    end of class tuesday

--- codevolume1.sql
+++ codevolume1.sql
@@ -2,5 +2,18 @@
 Phil's SQL code
 let's see how it plays out */
 
-SELECT *
-FROM app_store_apps;
+SELECT name, a.price AS app_price, 
+p.price::money::decimal AS play_price, 
+a.rating AS app_rating, 
+p.rating AS play_rating, 
+a.review_count AS app_reviews, 
+p.review_count AS play_reviews, 
+p.content_rating, 
+p.genres as play_genre,
+a.primary_genre as app_genre,
+trim(trailing '+' from p.install_count) AS p.play_installs
+FROM App_store_apps AS a
+	INNER JOIN play_store_apps AS p
+	USING (name)
+	order by play_price desc
+	cast(p.play_installs as int)
