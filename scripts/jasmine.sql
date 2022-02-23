fd31d099efdbfe70ba74558e59088fe73473d3b0
Jasmine Drumright <jmdrumright@gmail.com>
2022-02-22 21:29:42 -0600

    Kamals code with price filter

--- scripts/jasmine.sql
+++ scripts/jasmine.sql
@@ -1,6 +1,13 @@
-SELECT *
-FROM play_store_apps
-WHERE rating IS NOT NULL
-AND content_rating <> 'Mature 17+'
-ORDER BY rating DESC
-LIMIT 10;
+SELECT name, 
+	a.price AS app_price, 
+	p.price::money::decimal AS play_price,
+	a.rating AS app_rating,
+	p.rating AS play_rating,
+	a.review_count AS app_reviews,
+	p.review_count AS play_reviews,
+	p.content_rating,
+	p.genres
+FROM App_store_apps AS a
+	INNER JOIN play_store_apps AS p
+	USING (name)
+WHERE a.price = 0 AND p.price::money::decimal = 0
