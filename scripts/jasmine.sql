6653d3d4aec258f1589b9ab7651262ea2b0a0d7d
Jasmine Drumright <jmdrumright@gmail.com>
2022-02-24 21:20:27 -0600

    Group code plus my subquery to chart in Excel

--- scripts/jasmine.sql
+++ scripts/jasmine.sql
@@ -1,24 +1,50 @@
---Iulia's full code:
+--Group full code (from Iulia):
 SELECT DISTINCT a.name,
-	a.price as app_price,
-	p.price::money::decimal as game_price,
-	a.rating as app_rating,
-	p.rating as game_rating,
-	a.primary_genre as app_store_genre,
-	p.genres as play_store_genre,
-	round((a.rating+p.rating)/2*2+1,1) lifespan_years,
-	(((round((a.rating+p.rating)/2*2+1,0))*12)*5000) as estimated_revenue,
-	(((round((a.rating+p.rating)/2*2+1,0))*12)*1000)+10000 as estimated_spending,
-	((((round((a.rating+p.rating)/2*2+1,0))*12)*5000))
-	-(((round((a.rating+p.rating)/2*2+1,0))*12)*1000)+10000 as total_revenue
-FROM app_store_apps as a
-JOIN play_store_apps as p
+	a.price AS app_price,
+	p.price::money::decimal AS game_price,
+	a.rating AS app_rating,
+	p.rating AS game_rating,
+	a.primary_genre AS app_store_genre,
+	p.genres AS play_store_genre,
+	(ROUND((a.rating+p.rating)/2*2+1,1)) AS lifespan_years,
+	(((ROUND((a.rating+p.rating)/2*2+1,0))*12)*5000) AS estimated_revenue,
+	(((ROUND((a.rating+p.rating)/2*2+1,0))*12)*1000)+10000 AS estimated_spending,
+	((((ROUND((a.rating+p.rating)/2*2+1,0))*12)*5000))
+	-(((ROUND((a.rating+p.rating)/2*2+1,0))*12)*1000)+10000 AS total_revenue
+FROM app_store_apps AS a
+JOIN play_store_apps AS p
 ON a.name = p.name
 WHERE a.price <= 1 AND p.price::money::decimal <= 1
-AND CAST(a.review_count AS decimal) >=100
-AND CAST(p.review_count AS decimal) >= 100
-AND a.rating >= 3.5 AND p.rating>= 3.5
+	AND CAST(a.review_count AS decimal) >=100
+	AND CAST(p.review_count AS decimal) >= 100
+	AND a.rating >= 3.5 AND p.rating>= 3.5
 ORDER BY lifespan_years DESC;
+
+--Subquery to pull lifespan_total_rev because select it didn't work
+SELECT app,
+	lifespan_years * total_revenue AS ls_total_rev
+FROM
+	(SELECT DISTINCT a.name AS app,
+		a.price AS app_price,
+		p.price::money::decimal AS game_price,
+		a.rating AS app_rating,
+		p.rating AS game_rating,
+		a.primary_genre AS app_store_genre,
+		p.genres AS play_store_genre,
+		(ROUND((a.rating+p.rating)/2*2+1,1)) AS lifespan_years,
+		(((ROUND((a.rating+p.rating)/2*2+1,0))*12)*5000) AS estimated_revenue,
+		(((ROUND((a.rating+p.rating)/2*2+1,0))*12)*1000)+10000 AS estimated_spending,
+		((((ROUND((a.rating+p.rating)/2*2+1,0))*12)*5000))
+		-(((ROUND((a.rating+p.rating)/2*2+1,0))*12)*1000)+10000 AS total_revenue
+	FROM app_store_apps AS a
+	JOIN play_store_apps AS p
+	ON a.name = p.name
+	WHERE a.price <= 1 AND p.price::money::decimal <= 1
+		AND CAST(a.review_count AS decimal) >=100
+		AND CAST(p.review_count AS decimal) >= 100
+		AND a.rating >= 3.5 AND p.rating>= 3.5
+	ORDER BY lifespan_years DESC) AS subquery
+ORDER BY ls_total_rev DESC;
 
 --Jasmine: Rating desc
 SELECT DISTINCT a.name, 
@@ -39,19 +65,6 @@ JOIN play_store_apps AS p
 USING (name)
 WHERE (a.price <= 1 AND p.price::money::decimal <= 1)
 ORDER BY estimated_revenue DESC, lifespan_years DESC, play_rating DESC
-LIMIT 13
-
-/* Top 10 Apps based on above query:
-1. PewDiePie's Tuber Simulator
-2. Domino's Pizza USA
-3. ASOS
-4. Egg, Inc.
-5. The Guardian
-6. Geometry Dash Lite
-7. Fernanfloo
-8. Toy Blast
-9. Narcos: Cartel Wars
-10. Zombie Catchers */
 
 --Kamal: Joining tables for free
 SELECT name, 
