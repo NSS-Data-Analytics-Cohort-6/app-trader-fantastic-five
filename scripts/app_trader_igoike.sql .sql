47438137f044ece10f87542693bf5d926f548b54
Iulia Goike <iulia.isaeva@gmail.com>
2022-02-24 20:13:26 -0600

    completed code

--- scripts/app_trader_igoike.sql .sql
+++ scripts/app_trader_igoike.sql .sql
@@ -1,9 +1,22 @@
-select a.name,
+select 
+DISTINCT a.name,
 a.price as app_price,
 p.price::money::decimal as game_price,
 a.rating as app_rating,
-p.rating as game_rating
+p.rating as game_rating,
+a.primary_genre as app_store_genre,
+p.genres as play_store_genre,
+round((a.rating+p.rating)/2*2+1,1) lifespan_years,
+(((round((a.rating+p.rating)/2*2+1,0))*12)*5000) as estamated_revenue,
+(((round((a.rating+p.rating)/2*2+1,0))*12)*1000)+10000 as estemated_spending,
+((((round((a.rating+p.rating)/2*2+1,0))*12)*5000))
+-(((round((a.rating+p.rating)/2*2+1,0))*12)*1000)+10000 as
+total_revenue
 from app_store_apps as a
-INNER JOIN play_store_apps as p
-USING (name)
-WHERE a.price <= 1 AND p.price::money::decimal <= 1;
+JOIN play_store_apps as p
+ON a.name = p.name
+WHERE a.price <= 1 AND p.price::money::decimal <= 1
+AND CAST(a.review_count as decimal) >=100 
+AND CAST(p.review_count as decimal) >= 100
+AND a.rating >= 3.5 AND p.rating>= 3.5
+order by lifespan_years DESC;
