b3eb616e86f63144fb7c55a9a94b0018b49c320e
Phil George <philipwgeorge@gmail.com>
2022-02-26 12:24:31 -0500

    Added in the array! Neat!

--- scripts/codevolume1.sql
+++ scripts/codevolume1.sql
@@ -1,9 +1,10 @@
 /* this is the beginning of 
 Phil's SQL code
-let's see how it plays out */
+let's see how it plays out
+You can hit F5 to only run the selected code!!!*/
 
-SELECT distinct p.name, 
-a.price AS app_price, 
+SELECT distinct trim(a.name) as application,
+a.price,
 p.price::money::decimal AS play_price, 
 a.rating AS app_rating, 
 p.rating AS play_rating, 
@@ -17,18 +18,37 @@ round((a.rating+p.rating)/2*2+1,0) as lifespan_years,
 round((cast(a.review_count as int) + p.review_count)/(p.review_count / (cast(replace(trim(trailing '+' from p.install_count), ',','') as numeric))),0) as estimated_downloads
 FROM App_store_apps AS a
 	INNER JOIN play_store_apps AS p
-	on a.name = p.name
-	WHERE a.price <= 1 AND p.price::money::decimal <= 1 
-	and p.name like '%Pizza%' or p.name like '%pizza'
-	or p.name like '%Pie%' or p.name like '%pie%'
-	or p.name like '%Egg%' or p.name like '%egg%'
-	or p.name like '%Fruit%' or p.name like '%fruit%'
-	or p.name like '%Flour%' or p.name like '%flour%'
-	or p.name like '%Recipe%' or p.name like '%recipe%'
-	or p.name like '%Cook%' or p.name like '%cook%'
-	or p.name like '%Farm%' or p.name like '%farm%'
-	or p.name like '%Bake%' or p.name like '%bake%'
-AND CAST(a.review_count as decimal) >=100
+	on trim(a.name) = trim(p.name)
+	WHERE a.price = p.price::money::decimal and a.price=0 and p.price::money::decimal=0
+	-- how can i get this to not be dumb looking
+	and a.name ilike any(array['%farm%','%pizza%','%egg%','%fruit%','%flour%','%recipe%','%dessert%','%cook%','%farm%','%bake%','%PAC-MAN%'])
+AND CAST(a.review_count as decimal) >= 100
 AND CAST(p.review_count as decimal) >= 100
 AND a.rating >= 3.5 AND p.rating>= 3.5
 order by lifespan_years DESC;
+
+-- take two down here
+
+SELECT distinct trim(upper(a.name)) as application,
+a.rating,
+cast(a.review_count as int),
+case when a.price = 0 then a.price
+else null end as app_price
+from app_store_apps as a
+inner join play_Store_apps as p
+on trim(upper(a.name)) = trim(upper(p.name))
+where a.price = 0 and p.price::money::decimal = 0
+and a.name ilike '%Pizza%'
+	or p.name ilike '%Pie%' 
+	or p.name ilike '%Egg%' 
+	or p.name ilike '%Fruit%' 
+	or p.name ilike '%Flour%' 
+	or p.name ilike '%Recipe%' 
+	or p.name ilike '%Dessert%' 
+	or p.name ilike '%Calculat%' 
+	or p.name ilike '%Butter%'
+	or p.name ilike '%Cook%' 
+	or p.name ilike '%Farm%' 
+	or p.name ilike '%Bake%' 
+	or p.name ilike '%PAC-MAN%'
+order by review_count desc;
